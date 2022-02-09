a680b1a43b8a10eb4d8845d7dc7c417197ea6a86
mslmyrtd <mslmyrtd@gmail.com>
2022-02-09 15:01:02 +0300

    user tablosu inherit edildi admin.py de

--- users/models.py
+++ users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    portfolio = models.URLField(blank=True)
+
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
