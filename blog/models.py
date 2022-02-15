0e9141c7b074acecf3d6b94446321b49fa2b3d7b
mslmyrtd <mslmyrtd@gmail.com>
2022-02-15 13:10:22 +0300

    models update

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Card(models.Model):
-    
-    title=models.CharField(max_length=15)
-    description=models.TextField()
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    # category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    image=models.ImageField()
+    publish_date=models.DateTimeField(auto_now_add=True)
+    last_updated=models.DateTimeField(auto_now=True)
+    author=models.ForeignKey()
+    status=
     
 def __str__(self):
     return self.title
+
+
