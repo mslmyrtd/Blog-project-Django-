305c9c1e1d34ead45211397241e591ad598124a0
mslmyrtd <mslmyrtd@gmail.com>
2022-02-16 13:03:25 +0300

    baştan başlayalım

--- users/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class UserForm(UserCreationForm):
-    class Meta():
-        model = User
-        
-        fields = ('username', 'email', 'password1', 'password2')
-        
