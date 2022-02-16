dd40cefad259dee6b1265f00657b54fb48920ca4
mslmyrtd <mslmyrtd@gmail.com>
2022-02-16 13:47:26 +0300

    added Category class

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-class Category():
-    pass
+from django.contrib.auth.models import User
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    
 # Create your models here.
 class Post(models.Model):
     OPTIONS=(
@@ -13,6 +15,6 @@ class Post(models.Model):
     category=models.ForeignKey(Category, on_delete=models.PROTECT)
     last_updated=models.DateTimeField(auto_now=True)
     published_date=models.DateTimeField(auto_now_add=True)
-    # author=models.ForeignKey(User,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
     status=models.CharField(max_length=10, choices=OPTIONS,default="d")
-    slug=
+    slug= models.SlugField(blank=True,unique=True)
