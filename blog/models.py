74b6664a0602657c3d7c10219d40410c2b3ab452
mslmyrtd <mslmyrtd@gmail.com>
2022-02-16 14:05:10 +0300

    superuser added str added

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name=models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
+        
+    class Meta:
+        verbose_name_plural="Categories"
+    
 # Create your models here.
 class Post(models.Model):
     OPTIONS=(
@@ -18,3 +24,7 @@ class Post(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     status=models.CharField(max_length=10, choices=OPTIONS,default="d")
     slug= models.SlugField(blank=True,unique=True)
+    
+    def __str__(self):
+        return self.title
+    
