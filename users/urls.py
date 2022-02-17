656ae7d72799d68d5ec1313b9be6d32dc80caf42
mslmyrtd <mslmyrtd@gmail.com>
2022-02-17 10:48:51 +0300

    urls added

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-
+from .views import home
 
  
  
- urlpatterns = [
-    #  path('', home, name="home")
+urlpatterns = [
+    
+     
+    path('', home, name="home"),
+    path('register/', home, name="register"),
+    path('logout', home, name="logout"),
+    path('login', home, name="login"),
+    path('profile', home, name="profile"),
+    path('create', home, name="create"),
 
     
 ]
