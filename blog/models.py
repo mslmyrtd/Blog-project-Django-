dc6ddb7cc3c023c6a372c9a9fd92b6938c632c60
mslmyrtd <mslmyrtd@gmail.com>
2022-02-18 21:28:50 +0300

    like comment count added

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,8 @@
 from email.policy import default
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.conf import settings
-def user_directory_path(instance,filename):
-    # return "blog/{0}/{1}".format(instance.author.id, filename)
-    pass
+
 class Category(models.Model):
     
     name=models.CharField(max_length=100)
@@ -24,16 +22,24 @@ class Post(models.Model):
     )
     title=models.CharField(max_length=100)
     content=models.TextField()
-    image=models.ImageField(upload_to="img/", blank=True)
+    image=models.ImageField(upload_to="img/", default="django.png")
     category=models.ForeignKey(Category, on_delete=models.PROTECT)
     last_updated=models.DateTimeField(auto_now=True)
     published_date=models.DateTimeField(auto_now_add=True)
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     status=models.CharField(max_length=10, choices=OPTIONS,default="d")
+    def comment_count(self):
+        return self.comment_set.all().count()
+    def like_count(self):
+        return self.like_set.all().count()
+    def postview_count(self):
+        return self.postview_set.all().count()
+   
     
     
     def __str__(self):
         return self.title
+   
     
     
 class Comment(models.Model):
