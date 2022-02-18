1427caeea41cf374fdc980feb3209413f60e8a9f
mslmyrtd <mslmyrtd@gmail.com>
2022-02-18 15:42:32 +0300

    blog models updated

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,53 @@
 from email.policy import default
 from django.db import models
-from django.contrib.auth.models import User
-
+# from django.contrib.auth.models import User
+from django.conf import settings
 def user_directory_path(instance,filename):
     # return "blog/{0}/{1}".format(instance.author.id, filename)
     pass
 class Category(models.Model):
-    pass
-    # name=models.CharField(max_length=100)
     
-    # def __str__(self):
-    #     return self.name
+    name=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
         
-    # class Meta:
-    #     verbose_name_plural="Categories"
+    class Meta:
+        verbose_name_plural="Categories"
     
 # Create your models here.
 class Post(models.Model):
-    pass
-    # OPTIONS=(
-    #     ("d","Draft"),
-    #     ("p","Published"),
-    # )
-    # title=models.CharField(max_length=100)
-    # content=models.TextField()
-    # image=models.ImageField(upload_to="img/", blank=True)
-    # category=models.ForeignKey(Category, on_delete=models.PROTECT)
-    # last_updated=models.DateTimeField(auto_now=True)
-    # published_date=models.DateTimeField(auto_now_add=True)
-    # author=models.ForeignKey(User,on_delete=models.CASCADE)
-    # status=models.CharField(max_length=10, choices=OPTIONS,default="d")
-    # slug= models.SlugField(blank=True,unique=True)
     
-    # def __str__(self):
-    #     return self.title
+    OPTIONS=(
+        ("d","Draft"),
+        ("p","Published"),
+    )
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    image=models.ImageField(upload_to="img/", blank=True)
+    category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    last_updated=models.DateTimeField(auto_now=True)
+    published_date=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    status=models.CharField(max_length=10, choices=OPTIONS,default="d")
     
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Comment(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    content=models.TextField()
+    
+    
+class Like(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    
+class PostView(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    time_stamp=models.DateTimeField(auto_now_add=True)
