099235bb631bba559c2ed6a02a285ecf2aed8767
mslmyrtd <mslmyrtd@gmail.com>
2022-02-18 13:13:57 +0300

    forms added model updated

--- blog/models.py
+++ blog/models.py
@@ -3,32 +3,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 def user_directory_path(instance,filename):
-    return "blog/{0}/{1}".format(instance.author.id, filename)
+    # return "blog/{0}/{1}".format(instance.author.id, filename)
+    pass
 class Category(models.Model):
-    name=models.CharField(max_length=100)
+    pass
+    # name=models.CharField(max_length=100)
     
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
         
-    class Meta:
-        verbose_name_plural="Categories"
+    # class Meta:
+    #     verbose_name_plural="Categories"
     
 # Create your models here.
 class Post(models.Model):
-    OPTIONS=(
-        ("d","Draft"),
-        ("p","Published"),
-    )
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    image=models.ImageField(upload_to="img/", blank=True)
-    category=models.ForeignKey(Category, on_delete=models.PROTECT)
-    last_updated=models.DateTimeField(auto_now=True)
-    published_date=models.DateTimeField(auto_now_add=True)
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
-    status=models.CharField(max_length=10, choices=OPTIONS,default="d")
-    slug= models.SlugField(blank=True,unique=True)
+    pass
+    # OPTIONS=(
+    #     ("d","Draft"),
+    #     ("p","Published"),
+    # )
+    # title=models.CharField(max_length=100)
+    # content=models.TextField()
+    # image=models.ImageField(upload_to="img/", blank=True)
+    # category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    # last_updated=models.DateTimeField(auto_now=True)
+    # published_date=models.DateTimeField(auto_now_add=True)
+    # author=models.ForeignKey(User,on_delete=models.CASCADE)
+    # status=models.CharField(max_length=10, choices=OPTIONS,default="d")
+    # slug= models.SlugField(blank=True,unique=True)
     
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
     
---
+++ users/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    class Meta():
+        model=User
+        fields=("username", "email", "password1", "password2", "portfolio","profile_pic", "first_name", "last_name")
